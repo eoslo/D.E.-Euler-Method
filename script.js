2405b25790f84e5d84e27545694e28748393e9b3
lluciano <lucio.luciano@mercadolibre.com>
2019-06-08 18:50:31 -0300

    chart

--- script.js
+++ script.js
@@ -25,14 +25,16 @@
 
         // if simple Euler
         if (selectBox.value == 1) {
+            let valores = []
             for (var j = 1; j <= n; j++) {
                 x = x + h;
                 // eval() is evil, but we need this now.
                 k = h * eval(getById("mainEquation").value);
                 y = y + k;
-
+                valores.push({x: x, y: y});
                 tablePrint += "<tr><td>" + j + "</td><td>" + Math.round(x * 100) / 100 + "</td><td>" + Math.round(y * 1000) / 1000 + "</td></tr>";
             }
+            renderizar(valores);
         }
 
         if (selectBox.value == 2) {
@@ -48,7 +50,7 @@
             var fX = finalX.value;
 
             var m1, m2;
-
+            let valores = [];
             for (i = 1; xArr[i - 1] < fX; i++) {
                 w = 100.0;
                 xArr[i] = xArr[i - 1] + h;
@@ -63,8 +65,10 @@
                     c = c + 1;
                 }
                 yArr[i] = s[c];
+                valores.push({x: xArr[i], y: yArr[i]});
             }
             console.log("The respective values of xArr and yArr are\n     xArr yArr");
+            renderizar(valores)
             for (j = 0; j < i; j++) {
                 // answer at last. Aaah.
                 tablePrint += "<tr><td>" + (j + 1) + "</td><td>" + xArr[j] + "</td><td>" + yArr[j] + "</td></tr>";
@@ -74,6 +78,7 @@
         if (selectBox.value == 3) {
             var fX = finalX.value;
             var i = 0;
+            let valores = [];
             while (x < fX) {
                 m1 = eulerFunction(x, y);
                 m2 = eulerFunction((x + h / 2), (y + m1 * h / 2));
@@ -83,8 +88,10 @@
                 y = y + m * h;
                 x = x + h;
                 i++;
+                valores.push({x: x, y: y});
                 tablePrint += "<tr><td>" + i + "</td><td>" + Math.round(x * 100) / 100 + "</td><td>" + Math.round(y * 1000) / 1000 + "</td></tr>";
             }
+            renderizar(valores);
         }
 
         getById("tableBody").innerHTML = tablePrint;
@@ -106,5 +113,46 @@
         return document.getElementById(id);
     }
 
+    function renderizar(valores) {
+        console.log(valores)
+
+        var ctx = document.getElementById('grafico');
+        var grafico = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: valores,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 
 })(document, window);
