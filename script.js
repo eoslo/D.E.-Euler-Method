4e2b92610defcaaa6454ecdd10cc437e0f3cc5a8
lluciano <lucio.luciano@mercadolibre.com>
2019-06-09 16:28:15 -0300

    almost done

--- script.js
+++ script.js
@@ -5,14 +5,6 @@
     var finalX = getById('final_valueX');
     var calculateBtn = getById('calculate');
     var myChart;
-    // Display según metodo seleccionado
-    metodoSeleccionado.onchange = function() {
-        if (this.value == 1 || this.value == 2 || this.value == 3) {
-            finalX.style.display = "block";
-        } else {
-            finalX.style.display = "none";
-        }
-    };
 
     // Inicialización del Calculo 
     calculate.onclick = function() {
@@ -23,48 +15,31 @@
         var impresionTabla = "";
 
         // Si se selecciona Método de Euler:
-        if (metodoSeleccionado.value == 1) {
-            renderizar(euler(funcion, finalX.value, x, y, h), eulerMejorado(funcion, finalX.value, x, y, h));
-        }
+        renderizar(euler(funcion, finalX.value, x, y, h), eulerMejorado(funcion, finalX.value, x, y, h), r4(finalX.value, x, y, h, funcion));
 
-        // Si se selecciona Método de Euler (Mejorado):
-        if (metodoSeleccionado.value == 2) {
-            renderizar(eulerMejorado(funcion, finalX.value, x, y, h));
-        }
-
-        // Si se selecciona el método Runge Kutta
-        if (metodoSeleccionado.value == 3) {
-            var fX = finalX.value;
-            var i = 0;
-            let valores = [];
-            while (x < fX) {
-                m1 = funcionEuler(x, y);
-                m2 = funcionEuler((x + h / 2), (y + m1 * h / 2));
-                m3 = funcionEuler((x + h / 2), (y + m2 * h / 2));
-                m4 = funcionEuler((x + h), (y + m3 * h));
-                m = ((m1 + 2 * m2 + 2 * m3 + m4) / 6);
-                y = y + m * h;
-                x = x + h;
-                i++;
-                valores.push({x:x,y:y});
-                impresionTabla += "<tr><td>" + i + "</td><td>" + Math.round(x * 100) / 100 + "</td><td>" + Math.round(y * 1000) / 1000 + "</td></tr>";
-            }
-            renderizar(valores)
-        }
 
         getById("tableBody").innerHTML = impresionTabla;
     };
 
-    // Función de Euler individual:
-    function funcionEuler(x, y) {
-        result = (x - y) / (x + y);
-        return result;
-    }
-
-    function calcSlope(a, b) {
-        var c;
-        c = a * a + b;
-        return c;
+    function r4(finalX, x, y, h, funcion){
+        var fX = finalX;
+        console.log(finalX, x, x < fX)
+        var i = 0;
+        var valores = [];
+        while (x < fX) {
+            m1 = evaluar(y, x, funcion);
+            m2 = evaluar((y + m1 * h / 2), (x + h / 2), funcion);
+            m3 = evaluar((y + m2 * h / 2), (x + h / 2), funcion);
+            m4 = evaluar((y + m3 * h), (x + h),funcion);
+            m = ((m1 + 2 * m2 + 2 * m3 + m4) / 6);
+            y = y + m * h;
+            x = x + h;
+            i++;
+            console.log(m1, m2, m3, m4, m, y, x)
+            valores.push({x:x, y:y});
+        }
+        console.log(valores)
+        return valores;
     }
 
     function getById(id) {
@@ -118,7 +93,7 @@
                             });
     }
 
-    function renderizar(puntosMejorado, puntosEuler) {
+    function renderizar(puntosMejorado, puntosEuler, r4) {
         
         var ctx = document.getElementById('grafico').getContext('2d');
         myChart = new Chart(ctx, {
@@ -164,6 +139,15 @@
                                         label: "Euler",  
                                         data: puntosEuler,
                                         borderColor: 'blue',
+                                        borderWidth: 2,
+                                        fill: false,
+                                        showLine: true,
+                                        pointBackgroundColor: 'blue'
+                                    },
+                                    {
+                                        label: "R4",  
+                                        data: r4,
+                                        borderColor: 'orange',
                                         borderWidth: 2,
                                         fill: false,
                                         showLine: true,
